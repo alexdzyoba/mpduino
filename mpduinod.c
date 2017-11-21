ba8e5813b5a59b58737f35be109820ac9a1e03e7
Alex Dzyoba <alex@dzyoba.com>
2017-11-22 00:19:47 +0300

    Restyle with clang-format

--- mpduinod.c
+++ mpduinod.c
@@ -1,28 +1,27 @@
 #include <fcntl.h>
-#include <termios.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include <signal.h>
-#include <libmpd-1.0/libmpd/libmpd.h>
-
 #include <syslog.h>
+#include <termios.h>
+
+#include <libmpd-1.0/libmpd/libmpd.h>
 
 #define CONNECTION_TIMEOUT 0
 
-#define VOLDOWN	2
-#define PREV 	3
-#define STOP 	4
-#define PLAY 	5
-#define NEXT 	6
-#define VOLUP 	7
+#define VOLDOWN 2
+#define PREV 3
+#define STOP 4
+#define PLAY 5
+#define NEXT 6
+#define VOLUP 7
 
 /* File descriptor for serial port */
 int fd;
 
 /* MPD object */
-MpdObj *obj = NULL;	
+MpdObj *obj = NULL;
 
 /* MPD hostname */
 char *host;
@@ -31,138 +30,148 @@ char *host;
 int port;
 
 /* Priority for syslog messages */
-int priority = LOG_CONS|LOG_USER;
-
+int priority = LOG_CONS | LOG_USER;
 
 void signal_handler(int sig)
 {
-	switch(sig)
-	{
-		case SIGQUIT:
-		case SIGTERM:
-			syslog(priority, "Caught SIGTERM. Exiting...");
-			free(host);
-			close(fd);
-			mpd_free(obj);
-			syslog(priority, "halted");
-			exit(0);
-			break;
-		default:
-			syslog(priority, "Caught unhandled signal\n");
-			break;
-	}
-	return;
+    switch (sig) {
+        case SIGQUIT:
+        case SIGTERM:
+            syslog(priority, "Caught SIGTERM. Exiting...");
+            free(host);
+            close(fd);
+            mpd_free(obj);
+            syslog(priority, "halted");
+            exit(0);
+            break;
+        default:
+            syslog(priority, "Caught unhandled signal\n");
+            break;
+    }
+    return;
 }
 
 void usage()
 {
-	printf("Usage: mpduinod <HOST> <PORT>\n");
-	return;
+    printf("Usage: mpduinod <HOST> <PORT>\n");
+    return;
 }
 
-void error_callback(MpdObj *mi,int errorid, char *msg, void *userdata)
+void error_callback(MpdObj *mi, int errorid, char *msg, void *userdata)
 {
-        syslog(priority, "Error %i:'%s'\n", errorid, msg);
+    syslog(priority, "Error %i:'%s'\n", errorid, msg);
 }
 
 void configure_serial_port()
 {
-	struct termios options;
-    
-	/* Get the current options for the port...*/
+    struct termios options;
+
+    /* Get the current options for the port...*/
     tcgetattr(fd, &options);
 
     /* Set the baud rates to 9600  */
-    if(cfsetispeed(&options, B9600) || cfsetospeed(&options, B9600))
-		syslog(priority, "Error configuring port");
+    if (cfsetispeed(&options, B9600) || cfsetospeed(&options, B9600)) {
+        syslog(priority, "Error configuring port");
+    }
 
-	return;
+    return;
 }
- 
+
 void connect2mpd()
 {
-	/* Create mpd object */
-    obj = mpd_new(host, port, NULL); 
-	if(obj == NULL)
-		syslog(priority, "Error: can't create mpd object");
-	
-	/* Set timeout */
+    /* Create mpd object */
+    obj = mpd_new(host, port, NULL);
+    if (obj == NULL) {
+        syslog(priority, "Error: can't create mpd object");
+    }
+
+    /* Set timeout */
     mpd_set_connection_timeout(obj, 10);
 
-	/* Connect to MPD */
-	mpd_signal_connect_error(obj,(ErrorCallback)error_callback, NULL);
-	if(mpd_connect(obj))
-		syslog(priority, "Error: can't connect to mpd server");
-	else
-		syslog(priority, "Successfully connected to mpd server %s at %d", host, port);
+    /* Connect to MPD */
+    mpd_signal_connect_error(obj, (ErrorCallback)error_callback, NULL);
+    if (mpd_connect(obj)) {
+        syslog(priority, "Error: can't connect to mpd server");
+    } else {
+        syslog(priority, "Successfully connected to mpd server %s at %d", host,
+               port);
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	char buf[1];
-	int state;
-	
-	/* Open syslog	*/
-	openlog("mpduinod", LOG_CONS, LOG_USER);
-			
-	/* Set signal handler */
-	signal(SIGQUIT, signal_handler);
-	
-	/* Handle MPD hostname, port number and serial port*/
-	if(argc !=4)
-	{
-		usage();
-		exit(1);
-	}
+    char buf[1];
+    int  state;
 
-	host = (char *)malloc(strlen(argv[1]));
-	strncpy(host, argv[1], strlen(argv[1]));
-	port = atoi(argv[2]);
+    /* Open syslog */
+    openlog("mpduinod", LOG_CONS, LOG_USER);
 
-	/* Daemonize! */
-	if(daemon(0,1))
-		syslog(priority, "Can't daemonize\n");	
+    /* Set signal handler */
+    signal(SIGQUIT, signal_handler);
 
-	/* Open serial port for reading */
-	configure_serial_port();
-	fd = open(argv[3], O_RDONLY);
-	if(fd == -1)
-	{
-		syslog(priority, "Can't open serial port\n");
-		exit(1);
-	}
-	
+    /* Handle MPD hostname, port number and serial port*/
+    if (argc != 4) {
+        usage();
+        exit(1);
+    }
 
-	/* Enter infinite loop */	
-	while(1)
-	{
-		read(fd, buf, 1);
-		syslog(priority, "Received byte %d", buf[0]);
-		
-		/* Establish connection to mpd	*/
-		if(!mpd_check_connected(obj))
-		{
-			syslog(priority, "Connection not established: connecting");
-			connect2mpd();
-		}
+    host = (char *)malloc(strlen(argv[1]));
+    strncpy(host, argv[1], strlen(argv[1]));
+    port = atoi(argv[2]);
 
-		switch(buf[0])
-		{
-			case VOLDOWN	: 	mpd_status_set_volume(obj, mpd_status_get_volume(obj)-5); 	break;
-			case PREV		:	mpd_player_prev(obj);	break;
-			case STOP 		:	mpd_player_stop(obj);	break;	
-			case PLAY 		:	{ 
-									state = mpd_player_get_state(obj);
-									switch(state)
-									{
-										case MPD_PLAYER_PAUSE: 
-										case MPD_PLAYER_STOP:	mpd_player_play(obj); 	break;
-										case MPD_PLAYER_PLAY:	mpd_player_pause(obj);	break;
-									}
-									break;
-								}
-			case NEXT 		:	mpd_player_next(obj);	break;
-			case VOLUP 		:	mpd_status_set_volume(obj, mpd_status_get_volume(obj)+5);	break;
-		}
-	}
+    /* Daemonize! */
+    if (daemon(0, 1)) {
+        syslog(priority, "Can't daemonize\n");
+    }
+
+    /* Open serial port for reading */
+    configure_serial_port();
+    fd = open(argv[3], O_RDONLY);
+    if (fd == -1) {
+        syslog(priority, "Can't open serial port\n");
+        exit(1);
+    }
+
+    /* Enter infinite loop */
+    while (1) {
+        read(fd, buf, 1);
+        syslog(priority, "Received byte %d", buf[0]);
+
+        /* Establish connection to mpd */
+        if (!mpd_check_connected(obj)) {
+            syslog(priority, "Connection not established: connecting");
+            connect2mpd();
+        }
+
+        switch (buf[0]) {
+            case VOLDOWN:
+                mpd_status_set_volume(obj, mpd_status_get_volume(obj) - 5);
+                break;
+            case PREV:
+                mpd_player_prev(obj);
+                break;
+            case STOP:
+                mpd_player_stop(obj);
+                break;
+            case PLAY: {
+                state = mpd_player_get_state(obj);
+                switch (state) {
+                    case MPD_PLAYER_PAUSE:
+                    case MPD_PLAYER_STOP:
+                        mpd_player_play(obj);
+                        break;
+                    case MPD_PLAYER_PLAY:
+                        mpd_player_pause(obj);
+                        break;
+                }
+                break;
+            }
+            case NEXT:
+                mpd_player_next(obj);
+                break;
+            case VOLUP:
+                mpd_status_set_volume(obj, mpd_status_get_volume(obj) + 5);
+                break;
+        }
+    }
 }
--- sketch.pde
+++ sketch.pde
@@ -1,73 +1,63 @@
 #define DELAY 100
 
-#define VOLDOWN	2
-#define PREV 	3
-#define STOP 	4
-#define PLAY 	5
-#define NEXT 	6
-#define VOLUP 	7
+#define VOLDOWN 2
+#define PREV 3
+#define STOP 4
+#define PLAY 5
+#define NEXT 6
+#define VOLUP 7
 
 // Notification led
 int led = 13;
 
 int button_state = LOW;
 
-
 void setup()
 {
-	// initialize the serial communication: set baud rate
-	Serial.begin(9600);
-	
-	// Initialize buttons pin modes
-	for(int i=2; i<=7; i++)
-	{
-		pinMode(i,INPUT);
-	}
-	
-	// Initialize led pin mode
-	pinMode(led,OUTPUT);
-	
+    // initialize the serial communication: set baud rate
+    Serial.begin(9600);
+
+    // Initialize buttons pin modes
+    for (int i = 2; i <= 7; i++) {
+        pinMode(i, INPUT);
+    }
+
+    // Initialize led pin mode
+    pinMode(led, OUTPUT);
 }
 
 // Blink routine
 void blink()
 {
-	digitalWrite(led, HIGH);
-	delay(DELAY);
-	digitalWrite(led, LOW);
-	delay(DELAY);
-		
+    digitalWrite(led, HIGH);
+    delay(DELAY);
+    digitalWrite(led, LOW);
+    delay(DELAY);
 }
 
 void handlePin(int pin)
 {
-	// Read pin state
-	button_state = digitalRead(pin);
-	
-	// If there were high voltage, i.e. button was pressed
-	if(button_state == HIGH)
-	{
-		// blink led
-		blink();
-		
-		// Send throug serial port pin number
-		Serial.write(pin);
-	}
-	else
-	{
-		digitalWrite(led,LOW);
-	}
-	
+    // Read pin state
+    button_state = digitalRead(pin);
+
+    // If there were high voltage, i.e. button was pressed
+    if (button_state == HIGH) {
+        // blink led
+        blink();
+
+        // Send throug serial port pin number
+        Serial.write(pin);
+    } else {
+        digitalWrite(led, LOW);
+    }
 }
 
 // Main cycle
 void loop()
 {
-	int i;
-	// Ask each pin
-	for(i=2; i<=7; i++)
-	{
-		handlePin(i);
-	}
-		
+    int i;
+    // Ask each pin
+    for (i = 2; i <= 7; i++) {
+        handlePin(i);
+    }
 }
